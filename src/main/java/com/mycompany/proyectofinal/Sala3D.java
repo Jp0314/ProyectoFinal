7dd4c97aa323fdebd71fedf35919fcd9e5dd96eb
Jp0314 <jupecastanedaca@ittepic.edu.mx>
2023-11-30 12:15:21 -0600

    primer commit

--- src/main/java/com/mycompany/proyectofinal/Sala3D.java
+++ src/main/java/com/mycompany/proyectofinal/Sala3D.java
@@ -9,5 +9,34 @@ package com.mycompany.proyectofinal;
  * @author julio
  */
 public class Sala3D extends Sala2D{
-    private 
+    private String lentes;
+    
+    public Sala3D(){
+        this.lentes= null;
+    }
+    public Sala3D(String lentes, int numBoletos, int numSala, String horario, String pelicula, double costo) {
+        super(numBoletos, numSala, horario, pelicula, costo);
+        this.lentes = lentes;
+    }
+
+    public String getLentes() {
+        return lentes;
+    }
+
+    public void setLentes(String lentes) {
+        this.lentes = lentes;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Sala3D{");
+        sb.append("lentes=").append(lentes);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+    
+   
 }
